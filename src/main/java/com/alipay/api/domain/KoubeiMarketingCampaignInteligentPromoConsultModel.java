0ac17ef42a2df9258537db41684456b59e2a5a1a
scvzerng <443683059a>
2017-09-07 11:07:57 +0800

    智能营销活动默认值查询

--- src/main/java/com/alipay/api/domain/KoubeiMarketingCampaignInteligentPromoConsultModel.java
+++ src/main/java/com/alipay/api/domain/KoubeiMarketingCampaignInteligentPromoConsultModel.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.annotation.*;
 import com.alipay.api.AlipayObject;
 import com.alipay.api.internal.mapping.ApiField;
 import io.swagger.annotations.*;
+import org.hibernate.validator.constraints.NotBlank;
 
 /**
  * 智能营销活动咨询推荐接口
@@ -19,6 +20,7 @@ public class KoubeiMarketingCampaignInteligentPromoConsultModel extends AlipayOb
     /**
      * 商户和支付宝交互时，用于代表支付宝分配给商户ID
      */
+    @NotBlank
     @ApiModelProperty(notes = " 商户和支付宝交互时，用于代表支付宝分配给商户ID")
     @JSONField(alternateNames = "partnerId", name = "partner_id")
     private String partnerId;
@@ -26,6 +28,7 @@ public class KoubeiMarketingCampaignInteligentPromoConsultModel extends AlipayOb
     /**
      * 智能营销方案ID
      */
+    @NotBlank
     @ApiModelProperty(notes = " 智能营销方案ID")
     @JSONField(alternateNames = "planId", name = "plan_id")
     private String planId;
@@ -35,6 +38,7 @@ public class KoubeiMarketingCampaignInteligentPromoConsultModel extends AlipayOb
      */
     @ApiModelProperty(notes = " 营销模板id，全场级别的")
     @JSONField(alternateNames = "templateId", name = "template_id")
+    @NotBlank
     private String templateId;
 
     public String getPartnerId() {
