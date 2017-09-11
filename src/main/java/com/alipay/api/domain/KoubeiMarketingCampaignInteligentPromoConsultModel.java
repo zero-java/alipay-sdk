2cd34098d8bbb2adbddbc2a100e12c36728f1ccd
scvzerng <443683059a>
2017-09-11 14:13:11 +0800

    默认值获取接口更新

--- src/main/java/com/alipay/api/domain/KoubeiMarketingCampaignInteligentPromoConsultModel.java
+++ src/main/java/com/alipay/api/domain/KoubeiMarketingCampaignInteligentPromoConsultModel.java
@@ -1,45 +1,44 @@
 package com.alipay.api.domain;
 
+import java.util.List;
+
 import com.alibaba.fastjson.annotation.*;
 import com.alipay.api.AlipayObject;
 import com.alipay.api.internal.mapping.ApiField;
+import com.alipay.api.internal.mapping.ApiListField;
 import io.swagger.annotations.*;
-import org.hibernate.validator.constraints.NotBlank;
 
 /**
  * 智能营销活动咨询推荐接口
  *
  * @author auto create
- * @since 1.0, 2017-09-06 19:32:28
+ * @since 1.0, 2017-09-08 14:40:38
  */
 @ApiModel(description = " 智能营销活动咨询推荐接口")
 public class KoubeiMarketingCampaignInteligentPromoConsultModel extends AlipayObject {
 
-    private static final long serialVersionUID = 4544713768791752248L;
+    private static final long serialVersionUID = 3389223641924739762L;
 
     /**
      * 商户和支付宝交互时，用于代表支付宝分配给商户ID
      */
-    @NotBlank
     @ApiModelProperty(notes = " 商户和支付宝交互时，用于代表支付宝分配给商户ID")
     @JSONField(alternateNames = "partnerId", name = "partner_id")
     private String partnerId;
 
     /**
-     * 智能营销方案ID
+     * 当全场普通和单品普通方案时必传，体验方案不用传
      */
-    @NotBlank
-    @ApiModelProperty(notes = " 智能营销方案ID")
-    @JSONField(alternateNames = "planId", name = "plan_id")
-    private String planId;
+    @ApiModelProperty(notes = " 当全场普通和单品普通方案时必传，体验方案不用传")
+    @JSONField(alternateNames = "shopIds", name = "shop_ids")
+    private List<String> shopIds;
 
     /**
-     * 营销模板id，全场级别的
+     * 营销模板的编号，GENERAL_EXPERIENCE：全场体验；GENERAL_NORMAL：全场普通；ITEM_EXPERIENCE：单品体验；ITEM_NORMAL：单品普通
      */
-    @ApiModelProperty(notes = " 营销模板id，全场级别的")
-    @JSONField(alternateNames = "templateId", name = "template_id")
-    @NotBlank
-    private String templateId;
+    @ApiModelProperty(notes = " 营销模板的编号，GENERAL_EXPERIENCE：全场体验；GENERAL_NORMAL：全场普通；ITEM_EXPERIENCE：单品体验；ITEM_NORMAL：单品普通")
+    @JSONField(alternateNames = "templateCode", name = "template_code")
+    private String templateCode;
 
     public String getPartnerId() {
         return this.partnerId;
@@ -49,20 +48,20 @@ public class KoubeiMarketingCampaignInteligentPromoConsultModel extends AlipayOb
         this.partnerId = partnerId;
     }
 
-    public String getPlanId() {
-        return this.planId;
+    public List<String> getShopIds() {
+        return this.shopIds;
     }
 
-    public void setPlanId(String planId) {
-        this.planId = planId;
+    public void setShopIds(List<String> shopIds) {
+        this.shopIds = shopIds;
     }
 
-    public String getTemplateId() {
-        return this.templateId;
+    public String getTemplateCode() {
+        return this.templateCode;
     }
 
-    public void setTemplateId(String templateId) {
-        this.templateId = templateId;
+    public void setTemplateCode(String templateCode) {
+        this.templateCode = templateCode;
     }
 
 }
