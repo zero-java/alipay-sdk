c7d624dc3a6f124c1ed583bcb6fb163be633dbaf
scvzerng <443683059a>
2017-09-13 10:37:52 +0800

    swagger注解

--- src/main/java/com/alipay/api/response/KoubeiMarketingCampaignInteligentPromoConsultResponse.java
+++ src/main/java/com/alipay/api/response/KoubeiMarketingCampaignInteligentPromoConsultResponse.java
@@ -1,31 +1,35 @@
 package com.alipay.api.response;
 
+import com.alibaba.fastjson.annotation.*;
 import com.alipay.api.internal.mapping.ApiField;
 import com.alipay.api.domain.Inteligentpromo;
 
 import com.alipay.api.AlipayResponse;
+import io.swagger.annotations.*;
 
 /**
  * ALIPAY API: koubei.marketing.campaign.inteligent.promo.consult response.
- * 
+ *
  * @author auto create
  * @since 1.0, 2017-09-06 19:36:18
  */
+@ApiModel(description = " ALIPAY API: koubei.marketing.campaign.inteligent.promo.consult response.")
 public class KoubeiMarketingCampaignInteligentPromoConsultResponse extends AlipayResponse {
 
-	private static final long serialVersionUID = 4297459113536244482L;
+    private static final long serialVersionUID = 4297459113536244482L;
 
-	/** 
-	 * 智能营销方案咨询的模型
-	 */
-	@ApiField("promo")
-	private Inteligentpromo promo;
+    /**
+     * 智能营销方案咨询的模型
+     */
+    @ApiModelProperty(notes = " 智能营销方案咨询的模型")
+    private Inteligentpromo promo;
 
-	public void setPromo(Inteligentpromo promo) {
-		this.promo = promo;
-	}
-	public Inteligentpromo getPromo( ) {
-		return this.promo;
-	}
+    public void setPromo(Inteligentpromo promo) {
+        this.promo = promo;
+    }
+
+    public Inteligentpromo getPromo() {
+        return this.promo;
+    }
 
 }
