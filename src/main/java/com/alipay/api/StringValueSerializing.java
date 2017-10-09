53698d36c0c90db1579e9418f2bba9f9bdd997e8
shenyueqi <1013566157@qq.com>
2017-10-09 22:19:51 +0800

    StringValueSerializing 类修改

--- src/main/java/com/alipay/api/StringValueSerializing.java
+++ src/main/java/com/alipay/api/StringValueSerializing.java
@@ -14,18 +14,15 @@ import java.util.Map;
 public class StringValueSerializing implements ObjectSerializer {
     @Override
     public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType, int features) throws IOException {
+        if(object==null) return;
         if(!(object instanceof JSONObject)){
-            throw new IllegalArgumentException("无法为非JSONObject对象序列化"+StringValueSerializing.class.getName());
+            throw new IllegalArgumentException("无法为非JSONObject对象序列化fieldName:"+fieldName);
         }
         JSONObject json = (JSONObject) object;
         StringBuilder sb = new StringBuilder();
         for(Map.Entry<String,Object> entry :json.entrySet()){
             sb.append("\"").append(entry.getKey()).append("\"").append(":").append("\"").append(entry.getValue()).append("\"").append(",");
         }
-        if(sb.length()>0){
-            serializer.write("{"+sb.substring(0,sb.lastIndexOf(","))+"}");
-        }else {
-            serializer.write("{}");
-        }
+        serializer.write("{"+sb.substring(0,sb.lastIndexOf(","))+"}");
     }
 }
